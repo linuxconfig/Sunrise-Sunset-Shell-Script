4b02af049d79bc5cccecfa7bf495a89aadb6e702
Lubos Rendek <web@linuxconfig.org>
2020-06-12 14:24:00 +1000

    update

--- sunrise-sunset.sh
+++ sunrise-sunset.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
-# Author: Lubos Rendek web@linuxconfig.org
 
 # First obtain a location code from: https://weather.codes/search/
 
 # Insert your location. For example LOXX0001 is a location code for Bratislava, Slovakia
 location="LOXX0001"
+tmpfile=/tmp/$location.out
 
 # Obtain sunrise and sunset raw data from weather.com
-sun_times=$( lynx --dump  https://weather.com/weather/today/l/$location | grep "\* Sun" | sed "s/[[:alpha:]]//g;s/*//" )
-# Alternative curl/wget solution. In case you wish to use curl or wget instead of lynx comment the above line and uncomment one of the lines below:
-#sun_times=$( curl -s  https://weather.com/weather/today/l/$location | sed 's/<span/\n/g' | sed 's/<\/span>/\n/g'  | grep -E "dp0-details-sunrise|dp0-details-sunset" | tr -d '\n' | sed 's/>/ /g' | cut -d " " -f 4,8 )
-#sun_times=$( wget -qO-  https://weather.com/weather/today/l/$location | sed 's/<span/\n/g' | sed 's/<\/span>/\n/g'  | grep -E "dp0-details-sunrise|dp0-details-sunset" | tr -d '\n' | sed 's/>/ /g' | cut -d " " -f 4,8 )
-# Extract sunrise and sunset times and convert to 24 hour format
-sunrise=$(date --date="`echo $sun_times | awk '{ print $1}'` AM" +%R)
-sunset=$(date --date="`echo $sun_times | awk '{ print $2}'` PM" +%R)
+wget -q "https://weather.com/weather/today/l/$location" -O "$tmpfile"
+
+SUNR=$(grep -o '<p class="_-_-components-src-molecule-SunriseSunset-SunriseSunset--dateValue--3H780">.*am</p>' "$tmpfile" | grep -o '>.*</p>' | cut -c 2- | rev | cut -c5- | rev)
+SUNS=$(grep -o '<p class="_-_-components-src-molecule-SunriseSunset-SunriseSunset--dateValue--3H780">.*pm</p>' "$tmpfile" | grep -o '>.*</p>' | cut -f10,1 -d'>' | cut -c2- | rev | cut -c4- | rev)
+
+sunrise=$(date --date="$SUNR" +%R)
+sunset=$(date --date="$SUNS" +%R)
 
 # Use $sunrise and $sunset variables to fit your needs. Example:
 echo "Sunrise for location $location: $sunrise"
 echo "Sunset for location $location: $sunset"
-
-
-
